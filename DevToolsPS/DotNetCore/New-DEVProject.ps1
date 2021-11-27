e6fefb2d74c6ff4d2cd2c34395317b397def304e
stho32 <stho32github@gmail.com>
2021-11-27 15:52:19 +0100

    add notes about adding unit tests to a dotnet 6 project

--- DevToolsPS/DotNetCore/New-DEVProject.ps1
+++ DevToolsPS/DotNetCore/New-DEVProject.ps1
@@ -20,9 +20,17 @@ function New-DEVProject {
         dotnet new classlib
         Set-Location ..
 
+        mkdir "$Name.Console"
+        Set-Location "$Name.Console"
+        dotnet new console
+        Set-Location ..
+
         mkdir "$Name.BL.Tests"
         Set-Location "$Name.BL.Tests"
         dotnet new classlib
+        dotnet add package Microsoft.NET.Test.Sdk
+        dotnet add package xunit
+        dotnet add package xunit.runner.visualstudio
         Set-Location ..
 
         mkdir "$Name.Interfaces"
@@ -36,6 +44,7 @@ function New-DEVProject {
         Set-Location ..
 
         dotnet sln "./$Name.sln" add "./$Name.BL/"
+        dotnet sln "./$Name.sln" add "./$Name.Console/"
         dotnet sln "./$Name.sln" add "./$Name.BL.Tests/"
         dotnet sln "./$Name.sln" add "./$Name.Interfaces/"
         dotnet sln "./$Name.sln" add "./$Name.Classes/"
